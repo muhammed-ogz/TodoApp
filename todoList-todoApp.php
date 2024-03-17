3ccf4e31b8f2d1854e026fcb39b3de4e27691a82
muhammed-ogz <muhammednabioguz@gmail.com>
2024-03-17 11:44:33 +0300

    first commit

---
+++ todoList-todoApp.php
@@ -0,0 +1,22 @@
+// todo list project 
+/*
+    Klasör ve Routing yapısı 
+    Tema Entegrasyonu 
+    Veritabanı Bağlantısı 
+    Veritabanı Tabloları
+    Login işlemleri
+
+    - Todo Kategorileri 
+        * Listeleme
+        * Ekleme
+        * Güncelleme 
+        * Silme 
+    
+    - Todo İşlemleri
+        * Listeleme
+        * Ekleme
+        * Güncelleme 
+        * Silme
+    
+    -Todo İstatistikleri
+*/
